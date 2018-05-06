ae9cbeb1b96d710300f7e12f24ca32a54b7b3bdc
milad-abbbaszadeh <milad.abbaszadeh94@gmail.com>
2018-05-06 14:52:20 +0200

    please take look at start_notebook1.sh in Holoclean folder

--- tools/HoloClean/start_notebook.sh
+++ tools/HoloClean/start_notebook.sh
@@ -1,16 +1,10 @@
-#set the prepration for the Holoclean
-
-
-#source activate py27
-#source /etc/profile
-#source activate py27
 
 # Set & move to home directory
 source set_env.sh
 cd "$HOLOCLEANHOME"
 
 # Launch jupyter notebook!
-echo "Launching Jupyter Notebook..."
-jupyter notebook
+#echo "Launching Jupyter Notebook..."
+#jupyter notebook
 
-#python2 /home/milad/Desktop/HoloClean/tutorials/Holoclean_Hospital.py
+python2 ./run1.py
