a974b477b545c22b8983ce77871753e3ce686b4a
milad-abbbaszadeh <milad.abbaszadeh94@gmail.com>
2018-05-11 12:22:23 +0200

    please check now

--- tools/HoloClean/start_notebook.sh
+++ tools/HoloClean/start_notebook.sh
@@ -1,10 +1,14 @@
+#source activate py27Env
+#source /etc/profile
+#source activate py27Env
+
 
 # Set & move to home directory
 source set_env.sh
 cd "$HOLOCLEANHOME"
 
 # Launch jupyter notebook!
-#echo "Launching Jupyter Notebook..."
-#jupyter notebook
+echo "Launching Jupyter Notebook..."
+jupyter notebook
 
-python2 ./run1.py
+#python2 ./run1.py
